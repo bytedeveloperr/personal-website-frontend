1be8537f487c4d3b6953aef4177fa6c8303e8e53
Yusuf Abdulrahman <abdulrahmanyusuf125@gmail.com>
2021-01-25 10:17:41 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 
 <main>
 <h4>Abdulrahman Yusuf</h4>
-Backend Developer
+<p><a href="https://twitter.com/bytedeveloper_">@bytedeveloper_</a></p>
 
 <h4>About</h4>
 <p>I'm a Backend Developer, passionate about building and peeping into complex systems. I write Node.js primarily and in some cases, I write PHP.</p>
